f7c9b977f4ef4b485c9c6714e2256b03d2e5b181
josephethompson <96962002+josephethompson@users.noreply.github.com>
2022-01-02 00:47:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,8 @@ from telethon.tl.types import InputMessagesFilterPhotos
 from telethon.tl.types import MessageEntityMention
 from telethon.tl.types import MessageEntityMentionName
 
-# Reading Configs
 config = configparser.ConfigParser()
-config.read("config.ini")
+config.read("/home/joseph_ethompson01/ajac-telegram-bot/config.ini")
 
 # Setting configuration values
 api_id = config['Telegram']['api_id']
@@ -19,14 +18,15 @@ api_hash = str(config['Telegram']['api_hash'])
 bot_token = str(config['Telegram']['bot_token'])
 
 # Get user data
-user_data_file = open('Storage/userdata.json', 'r')
+user_data_file = open('/home/joseph_ethompson01/ajac-telegram-bot/Storage/userdata.json', 'r')
 all_user_data = json.load(user_data_file)
 
 #other variables
-PhysiqueFridayTag = "#FizeekFriday"
-BotResponses = ["GOOD JOB BROTHER", "WELL DONE", "WOOOO", "I SEE YOU!", "GAINNNS", "HYPERBOREAN"]
+PhysiqueCheckCommands = ["/fizeekcheck", "/physiquecheck"]
+PhysiqueFridayTags = ["FizeekFriday", "PhysiqueFriday", "Fizeek Friday", "Physique Friday", "PhysiquePost", "FizeekPost", "Fizeek Post", "Physique Post"]
+BotResponses = ["GOOD JOB BROTHER", "WELL DONE", "WOOOO", "I SEE YOU", "GAINNNS", "HYPERBOREAN", "CHADISTANIAN"]
 
-bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+bot = TelegramClient('bot1', api_id, api_hash).start(bot_token=bot_token)
 
 def create_user_data(user_id):
     user_key = str(user_id)
@@ -47,6 +47,11 @@ def get_user_from_username(user_name, users):
         if user.username == user_name:
             return user
 
+def get_user_from_userid(user_id, users):
+    for user in users:
+        if user.id == user_id:
+            return user
+
 async def get_last_physique_post(user_id, chat):
     user_data = get_user_data(user_id) or create_user_data(user_id)
     last_physique_post_id = user_data['lastPhysiqueFridayPostId']
@@ -54,43 +59,47 @@ async def get_last_physique_post(user_id, chat):
         message = await bot.get_messages(chat, ids=last_physique_post_id )
         return message
 
+async def check_physique(user, event):
+    chat = await event.get_chat()
+    last_physique_post = await get_last_physique_post(user.id, chat)
+    if last_physique_post != None:
+        await event.reply(f'{user.username or user.first_name} FIZEEK CHECK: \n\nLAST FIZEEK POST DATE:  {last_physique_post.date.strftime("%B %d, %Y")}\nLAST FIZEEK POST:')
+        await bot.send_message(chat, last_physique_post)
+    else:
+        await event.reply('NO FIZEEK FOUND')
+
 @bot.on(events.NewMessage)
 async def my_event_handler(event):
     chat = await event.get_chat()
     sender = await event.get_sender()
     sender_user_data = get_user_data(sender.id) or create_user_data(sender.id)
 
-    if PhysiqueFridayTag.lower() in event.raw_text.lower() and event.photo:
+    if "/help" in event.raw_text.lower():
+        await event.reply("COMMAND LIST: \n\n/physiquecheck [user] (/fizeekcheck [user])")
+    elif any(tag.lower() in event.raw_text.lower() for tag in PhysiqueFridayTags) and event.photo:
         sender_user_data['lastPhysiqueFridayPostId'] = event.message.id
         await event.reply(random.choice(BotResponses))
-    elif "/fizeekcheck" in event.raw_text.lower():
+    elif any(tag.lower() in event.raw_text.lower() for tag in PhysiqueCheckCommands):
         users = await bot.get_participants(chat)
         message = event.message
         entities = message.entities
 
+        mentioned_user = sender
         for entity in entities:
             if entity.__class__ is MessageEntityMention:
                 mentioned_user_name = event.raw_text[entity.offset+1:len(event.raw_text)]
                 mentioned_user = get_user_from_username(mentioned_user_name, users)
-                last_physique_post = await get_last_physique_post(mentioned_user.id, chat)
-                if last_physique_post != None:
-                    await event.reply(f'{mentioned_user.username or mentioned_user.first_name} FIZEEK CHECK: \n\nLAST FIZEEK POST DATE:  {last_physique_post.date.strftime("%B %d, %Y")}\nLAST FIZEEK POST:')
-                    await bot.send_message(chat, last_physique_post)
-                else:
-                    await event.reply('NO FIZEEK FOUND')
             elif entity.__class__ is MessageEntityMentionName:
-                last_physique_post = await get_last_physique_post(entity.user_id, chat)
-                if last_physique_post != None:
-                    await event.reply(f'{mentioned_user.username or mentioned_user.first_name} FIZEEK CHECK: \n\nLAST FIZEEK POST DATE:  {last_physique_post.date.strftime("%B %d, %Y")}\nLAST FIZEEK POST:')
-                    await bot.send_message(chat, last_physique_post)
-                else:
-                    await event.reply('NO FIZEEK FOUND')
+                mentioned_user = get_user_from_userid(entity.user_id, users)
+
+        await check_physique(mentioned_user, event)
+
 def main_update():
     while True:
         time.sleep(5)
 
         try:
-            with open("Storage/userdata.json", "w") as outfile:
+            with open("/home/joseph_ethompson01/ajac-telegram-bot/Storage/userdata.json", "w") as outfile:
                 json.dump(all_user_data, outfile)
         except:
             pass
